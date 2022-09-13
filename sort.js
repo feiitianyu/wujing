b4866efe815d9b6e69f99fa801c91c54252ab9ee
曹立泽 <caolize@shgbit.com>
2022-09-13 15:52:35 +0800

    fix insertionSort

--- sort.js
+++ sort.js
@@ -35,7 +35,7 @@ function insertionSort(arr) {
     return
   }
   for (let i = 1; i < arr.length; i++) {
-    for (let j = 0; j < i && arr[j] > arr[j+1]; j++) {
+    for (let j = i - 1; j >= 0 && arr[j] > arr[j+1]; j++) {
       swap(arr, j, j+1)
     }
   }
